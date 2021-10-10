956b53075ac25e5e7358b5bdd9f66176c8f9dfc0
沈雨涵 <yhshen2000@foxmail.com>
2021-10-10 12:18:40 +0800

    21/10/9每日一题

--- 1每日一题/LeetCode352.cpp
+++ 1每日一题/LeetCode352.cpp
@@ -1,3 +1,4 @@
+//维护set
 class SummaryRanges {
 public:
     set<pair<int, int>> se;
@@ -54,3 +55,57 @@ public:
  * obj->addNum(val);
  * vector<vector<int>> param_2 = obj->getIntervals();
  */
+
+//----------------------------------------------------------------
+
+//并查集
+class SummaryRanges {
+public:
+    int N = 1e4 + 10;
+
+    vector<int> p;
+    map<int, int> mp;
+
+    int find(int x) {
+        if (p[x] != x)
+            p[x] = find(p[x]);
+        return p[x];
+    }
+
+    SummaryRanges() {
+        p.resize(N);
+        for (int i = 0; i < N; i ++ )
+            p[i] = i;
+    }
+    
+    void addNum(int val) {
+        val ++ ;
+        if (mp.count(find(val))) return;
+        if (mp.count(find(val - 1)) && mp.count(val + 1)) {
+            p[val + 1] = find(val - 1);
+            p[val] = find(val - 1);
+            mp[find(val - 1)] = mp[val + 1];
+            mp.erase(val + 1);
+        }
+        else if (mp.count(find(val - 1))) {
+            p[val] = find(val - 1);
+            mp[find(val - 1)] = val;
+        }
+        else if (mp.count(val + 1)) {
+            p[val + 1] = val;
+            mp[val] = mp[val + 1];
+            mp.erase(val + 1);
+        }
+        else {
+            mp[val] = val;
+        }
+    }
+    
+    vector<vector<int>> getIntervals() {
+        vector<vector<int>> ans;
+        for (auto& [a, b] : mp) {
+            ans.push_back({(int)a - 1, (int)b - 1});
+        }
+        return ans;
+    }
+};

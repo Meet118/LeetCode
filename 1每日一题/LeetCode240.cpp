12bf859f75e7f42de688480f23e3b6f51e8b34a0
沈雨涵 <yhshen2000@foxmail.com>
2021-10-25 19:21:50 +0800

    21/10/25每日一题

---
+++ 1每日一题/LeetCode240.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int x = 0, y = m - 1;
+        while (x < n && y >= 0) {
+            if (matrix[x][y] < target)
+                x ++ ;
+            else if (matrix[x][y] > target)
+                y -- ;
+            else
+                return true;
+        }
+        return false;
+    }
+};

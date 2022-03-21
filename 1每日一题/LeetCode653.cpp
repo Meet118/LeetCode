b284f1f416c42c69fdf26d69bee2b9eaf1f6a7d7
沈雨涵 <yhshen2000@foxmail.com>
2022-03-21 10:46:09 +0800

    22/3/21每日一题

---
+++ 1每日一题/LeetCode653.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_set<int> se;
+    bool f = false;
+    int k;
+
+    void dfs(TreeNode* u) {
+        if (!u) return;
+        dfs(u->left);
+        int x = u->val;
+        if (se.count(k - x))
+            f = true;
+        se.insert(x);
+        dfs(u->right);
+    }
+
+    bool findTarget(TreeNode* root, int _k) {
+        k = _k;
+        dfs(root);
+        return f;
+    }
+};

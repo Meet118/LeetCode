7d3e392697f42d1db52f64c1ba32157c7ae7166c
沈雨涵 <yhshen2000@foxmail.com>
2022-10-10 13:49:07 +0800

    22/10/10每日一题

---
+++ 1每日一题/LeetCode801.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<vector<int>> f(n, vector<int>(2, 0x3f3f3f3f));
+        f[0][0] = 0;
+        f[0][1] = 1;
+        for (int i = 1; i < n; i ++ ) {
+            if (nums1[i - 1] < nums1[i] && nums2[i - 1] < nums2[i]) {
+                f[i][0] = f[i - 1][0];
+                f[i][1] = f[i - 1][1] + 1;
+            }
+            if (nums2[i - 1] < nums1[i] && nums1[i - 1] < nums2[i]) {
+                f[i][0] = min(f[i][0], f[i - 1][1]);
+                f[i][1] = min(f[i][1], f[i - 1][0] + 1);
+            }
+        }
+        return min(f[n - 1][0], f[n - 1][1]);
+    }
+};

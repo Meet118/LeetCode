264711e0e348c22e0b94271e0526a93200e00aa7
沈雨涵 <yhshen2000@foxmail.com>
2022-10-29 11:55:48 +0800

    22/10/29每日一题

---
+++ 1每日一题/LeetCode1773.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ans = 0;
+        for (auto item : items) {
+            if (ruleKey == "type" && item[0] == ruleValue) ans ++ ;
+            if (ruleKey == "color" && item[1] == ruleValue) ans ++ ;
+            if (ruleKey == "name" && item[2] == ruleValue) ans ++ ;
+        }
+        return ans;
+    }
+};

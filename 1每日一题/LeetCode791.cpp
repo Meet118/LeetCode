545921f015195297e141cabeb2b160258ac3e6ba
沈雨涵 <yhshen2000@foxmail.com>
2022-11-13 18:07:06 +0800

    22/11/13每日一题

---
+++ 1每日一题/LeetCode791.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> pos(26);
+        for (int i = 0; i < order.size(); i ++ ) pos[order[i] - 'a'] = i;
+        sort(s.begin(), s.end(), [&](char a, char b) {
+            return pos[a - 'a'] < pos[b - 'a'];
+        });
+        return s;
+    }
+};

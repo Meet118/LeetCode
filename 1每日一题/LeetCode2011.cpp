a86a1ad228161673f2e1880aaaa0a6003165e3cf
沈雨涵 <yhshen2000@foxmail.com>
2022-12-23 13:11:53 +0800

    22/12/23每日一题

---
+++ 1每日一题/LeetCode2011.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for (string s : operations) {
+            if (s[1] == '-') ans -- ;
+            else ans ++ ;
+        }
+        return ans;
+    }
+};

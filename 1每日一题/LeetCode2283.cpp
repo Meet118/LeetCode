b8c1c06f75c49329bfcc8dbc1a4ca5e146faaf1d
沈雨涵 <yhshen2000@foxmail.com>
2023-01-11 23:18:16 +0800

    23/1/11每日一题

---
+++ 1每日一题/LeetCode2283.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        vector<int> cnt(10);
+        for (char c : num) cnt[c - '0'] ++ ;
+        for (int i = 0; i < num.size(); i ++ )
+            if (num[i] - '0' != cnt[i])
+                return false;
+        return true;
+    }
+};
